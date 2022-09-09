0b633c4c59d39027e6f54f9fda49f6a3d2e610d0
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-09 13:01:48 -0300

    Inserindo fk na tabela relacional diretor_filme

--- js/1_create.js
+++ js/1_create.js
@@ -4,24 +4,24 @@ const db = require("./_database");
 async function createTables(){
     await db.connect()
     
-    // await db.query(`CREATE TABLE listaFilmes(
-    //     id serial PRIMARY KEY,
-    //     nome VARCHAR(100) UNIQUE NOT NULL,
-    //     diretor VARCHAR(100)  NOT NULL,
-    //     lançamento integer  NOT NULL,
-    //     atoratriz VARCHAR(100)  NOT NULL,
-    //     genero VARCHAR(100)  NOT NULL
-    // )`)
+    await db.query(`CREATE TABLE listaFilmes(
+        id serial PRIMARY KEY,
+        nome VARCHAR(100) UNIQUE NOT NULL,
+        diretor VARCHAR(100)  NOT NULL,
+        lançamento integer  NOT NULL,
+        atoratriz VARCHAR(100)  NOT NULL,
+        genero VARCHAR(100)  NOT NULL
+    )`)
 
-    // await db.query(`CREATE TABLE listaAtrizAtor(
-    //     id serial PRIMARY KEY,
-    //     nome VARCHAR(50) UNIQUE NOT NULL,
-    // )`)
+    await db.query(`CREATE TABLE listaAtrizAtor(
+        id serial PRIMARY KEY,
+        nome VARCHAR(50) UNIQUE NOT NULL,
+    )`)
 
-    // await db.query(`CREATE TABLE listaDir(
-    //     id serial PRIMARY KEY,
-    //     nome VARCHAR(50) UNIQUE NOT NULL,
-    // )`)
+    await db.query(`CREATE TABLE listaDir(
+        id serial PRIMARY KEY,
+        nome VARCHAR(50) UNIQUE NOT NULL,
+    )`)
 
     await db.query(`CREATE TABLE diretor_filme(
         diretor_id INTEGER NOT NULL,
