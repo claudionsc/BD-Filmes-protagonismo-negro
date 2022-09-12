6bc2e247a76e08cb47210e0c07ee5776104b7570
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-12 14:46:16 -0300

    insert tabela relacional filmes_diretor)

--- sequelize/2_insert.js
+++ sequelize/2_insert.js
@@ -4,84 +4,84 @@ async function insert(){
 
     // FILMES 
     
-    // const corra = await models.filme.create({
-    //     nome: 'Corra',
-    //     diretor: 'Jordan Peele',
-    //     lançamento: 2017,
-    //     atoratriz: 'Daniel Kaluuya',
-    //     genero: 'Suspense, terror'
-    // })
+    const corra = await models.filme.create({
+        nome: 'Corra',
+        diretor: 'Jordan Peele',
+        lançamento: 2017,
+        atoratriz: 'Daniel Kaluuya',
+        genero: 'Suspense, terror'
+    })
 
-    // const klan = await models.filme.create({
-    //     nome: 'Infiltrado na Klan',
-    //     diretor: 'Spike Lee',
-    //     lançamento: 2018,
-    //     atoratriz: 'John David Washington',
-    //     genero: 'Drama, biográfico, policial, suspense'
-    // })
+    const klan = await models.filme.create({
+        nome: 'Infiltrado na Klan',
+        diretor: 'Spike Lee',
+        lançamento: 2018,
+        atoratriz: 'John David Washington',
+        genero: 'Drama, biográfico, policial, suspense'
+    })
 
-    // const panteranegra = await models.filme.create({
-    //     nome: 'Pantera Negra',
-    //     diretor: 'Ryan Coogler',
-    //     lançamento: 2018,
-    //     atoratriz: 'Chadwick Boseman',
-    //     genero: 'Ação, aventura, ficção científica'
-    // })
+    const panteranegra = await models.filme.create({
+        nome: 'Pantera Negra',
+        diretor: 'Ryan Coogler',
+        lançamento: 2018,
+        atoratriz: 'Chadwick Boseman',
+        genero: 'Ação, aventura, ficção científica'
+    })
 
-    // const cidade = await models.filme.create({
-    //     nome: 'Cidade de Deus',
-    //     diretor: 'Fernando Meirelles',
-    //     lançamento: 2002,
-    //     atoratriz: 'Alexandre Rodrigues',
-    //     genero: 'Drama, ação'
-    // })
+    const cidade = await models.filme.create({
+        nome: 'Cidade de Deus',
+        diretor: 'Fernando Meirelles',
+        lançamento: 2002,
+        atoratriz: 'Alexandre Rodrigues',
+        genero: 'Drama, ação'
+    })
 
-    // const django = await models.filme.create({
-    //     nome: 'Django Livre',
-    //     diretor: 'Quentin Tarantino',
-    //     lançamento: 2012,
-    //     atoratriz: 'Jamie Foxx',
-    //     genero: 'Drama, ação, faroeste'
-    // })
+    const django = await models.filme.create({
+        nome: 'Django Livre',
+        diretor: 'Quentin Tarantino',
+        lançamento: 2012,
+        atoratriz: 'Jamie Foxx',
+        genero: 'Drama, ação, faroeste'
+    })
 
-    // const nos = await models.filme.create({
-    //     nome: 'Nós',
-    //     diretor: 'Jordan Peele',
-    //     lançamento: 2019,
-    //     atoratriz: `Lupita Nyong'o`,
-    //     genero: 'Suspense, ficção científica, terror'
-    // })
+    const nos = await models.filme.create({
+        nome: 'Nós',
+        diretor: 'Jordan Peele',
+        lançamento: 2019,
+        atoratriz: `Lupita Nyong'o`,
+        genero: 'Suspense, ficção científica, terror'
+    })
 
-    // const comptom = await models.filme.create({
-    //     nome: 'Straight Outta Compton',
-    //     diretor: 'F. Gary Gray',
-    //     lançamento: 2015,
-    //     atoratriz: `O'Shea Jackson Jr (e outros)`,
-    //     genero: 'Drama, biográfico, policial'
-    // })
+    const comptom = await models.filme.create({
+        nome: 'Straight Outta Compton',
+        diretor: 'F. Gary Gray',
+        lançamento: 2015,
+        atoratriz: `O'Shea Jackson Jr (e outros)`,
+        genero: 'Drama, biográfico, policial'
+    })
 
-    // const copiava = await models.filme.create({
-    //     nome: 'O Homem que Copiava',
-    //     diretor: 'Jorge Furtado',
-    //     lançamento: 2003,
-    //     atoratriz: 'Lázaro Ramos',
-    //     genero: 'Drama'
-    // })
+    const copiava = await models.filme.create({
+        nome: 'O Homem que Copiava',
+        diretor: 'Jorge Furtado',
+        lançamento: 2003,
+        atoratriz: 'Lázaro Ramos',
+        genero: 'Drama'
+    })
 
-    // const moonlight = await models.filme.create({
-    //     nome: 'Moonlight: Sob a Luz do Luar',
-    //     diretor: 'Barry Jenkins',
-    //     lançamento: 2017,
-    //     atoratriz: 'Trevante Rhodes',
-    //     genero: 'Drama'
-    // })
-    // const besouro = await models.filme.create({
-    //     nome: 'Besouro',
-    //     diretor: 'João Daniel Tikhomiroff',
-    //     lançamento: 2009,
-    //     atoratriz: 'Aílton Carmo',
-    //     genero: 'Ação, drama biográfico, fantasia'
-    // })
+    const moonlight = await models.filme.create({
+        nome: 'Moonlight: Sob a Luz do Luar',
+        diretor: 'Barry Jenkins',
+        lançamento: 2017,
+        atoratriz: 'Trevante Rhodes',
+        genero: 'Drama'
+    })
+    const besouro = await models.filme.create({
+        nome: 'Besouro',
+        diretor: 'João Daniel Tikhomiroff',
+        lançamento: 2009,
+        atoratriz: 'Aílton Carmo',
+        genero: 'Ação, drama biográfico, fantasia'
+    })
 
     // DIRETORES 
 
@@ -112,6 +112,18 @@ async function insert(){
     const joao = await  models.diretor.create({
         nome: 'João Danielk Tikhomiroff '
     })
+
+    // DIRETORES EM FILMES 
+    await jordan.setFilmes([corra, nos])
+    await spike.setFilmes([klan])
+    await ryan.setFilmes([panteranegra])
+    await meirelles.setFilmes([cidade])
+    await tarantino.setFilmes([django])
+    await Gary.setFilmes([comptom])
+    await furtado.setFilmes([copiava])
+    await jenkins.setFilmes([moonlight])
+    await joao.setFilmes([besouro])
+
 
 
     console.log('-->INSERIDOS<--')
